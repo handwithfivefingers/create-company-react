8b7d4c56e8bf10ee9a62870e388a872d081ae7b6
DESKTOP-GH4HFOL\HCM-PC-42 <truyenmv@ominext.com>
2022-06-14 15:22:26 +0700

    fix about

--- src/pages/Admin/AdminAbout/index.js
+++ src/pages/Admin/AdminAbout/index.js
@@ -1,16 +1,25 @@
-import React from "react";
-
+import React from 'react';
+import styles from './styles.module.scss';
 export default function AdminAbout() {
-  return <div>
-  Author: Truyền Mai
-  Contact: 079 834 123 9
-  Website Detail: 
-  - Version : 0.3
-  - Framework: Nodejs Reactjs
-  - Node version: 16
-  - React version: 17
-  - React Router: 6
-  - Express: 4.17
-  - 
-  </div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.author}>
+        <ul>
+          <li>Author: Truyền Mai</li>
+          <li>Contact: 079 834 123 9</li>
+        </ul>
+      </div>
+      <div className={styles.listItem}>
+        <ul>
+          Website Detail:
+          <li> Version : 0.4 (last updated: 14/06/2022)</li>
+          <li> Framework: Nodejs Reactjs</li>
+          <li> Node version: 16</li>
+          <li> Express: 4.17</li>
+          <li> React version: 17</li>
+          <li> React Router: 6</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
